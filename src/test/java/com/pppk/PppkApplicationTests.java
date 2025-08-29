9fe8f7d632d934d904c6d93d6b4f5c8d7d398a4c
lmiholic <lukas.miholiclol@gmail.com>
2025-08-29 14:27:29 +0200

    Initial commit

---
+++ src/test/java/com/pppk/PppkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pppk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PppkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
